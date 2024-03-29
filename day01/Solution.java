20132cd9468c3900bbac18c90d365ec6fff50de0
Hsegunn <segun9909@naver.com>
2024-03-29 09:19:58 +0900

    옷가게할인받기 프로그래머스

---
+++ day01/Solution.java
@@ -0,0 +1,20 @@
+// 옷가게 할인받기
+class Solution {
+    public int solution(int price) {
+        if(price >= 500000) {
+            price *= 0.8; // 50만원일 때, 20%할인
+        } 
+        else if(price >= 300000) {
+            price *= 0.9; // 30만원일 때, 10%할인
+        } 
+        else if(price >= 100000) {
+            price *= 0.95; // 10만원일 때, 5%할인
+        }
+        return price;
+    }      
+
+public static void main(String[] args) {
+    Solution sol = new Solution();
+    System.out.println(sol.solution(250000));
+}
+}
