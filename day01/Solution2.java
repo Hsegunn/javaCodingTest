4e99c0756b6ded38021178655d50dbd916cd2cc5
Hsegunn <segun9909@naver.com>
2024-03-30 15:19:27 +0900

    가위바위보 코딩테스트

---
+++ day01/Solution2.java
@@ -0,0 +1,29 @@
+class Solution2 {
+    public String solution(String rsp) {
+        String answer = "";
+   
+        char scissors = '2';
+        char rock = '0';
+        char paper = '5';
+        
+        for(int i = 0; i < rsp.length(); i++){
+            // chatAt()는 문자열에서 특정 위치에 있는 문자를 가져오는데 사용
+            if(rsp.charAt(i) == scissors){
+                answer += rock;
+            }
+            else if(rsp.charAt(i) == rock){
+                answer += paper;
+            }
+            else if(rsp.charAt(i) == paper){
+                answer += scissors;
+            }
+        }  
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution2 sol = new Solution2();
+        String input = "205";
+        String result = sol.solution(input);
+        System.out.println("Result: " + result);
+    }
+}
